c813dc19da3913f83ab43a97612aa97d24a63b3b
Taku Yamada <t-yamada@nautilus-technologies.com>
2012-04-16 14:03:07 +0900

    create app and rake db:create

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '07c53f7623fda2a1c491464ff8db334366163a9ce509d61879cf73ee047514892894852a8d684c2a3826e7681fce6e5d66770292a444da755b68e1dbf724f42f'
